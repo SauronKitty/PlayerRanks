5f2479fd7ee456cce0087d1ec9ab0c63fb8ec285
evilmaniac <evilmaniac@eM-SLP>
2015-03-06 12:59:52 -0600

    Begun adding Setters/Getters into PlayerNode

--- PlayerData/PlayerNode.php
+++ PlayerData/PlayerNode.php
@@ -44,22 +44,150 @@ class PlayerNode{
         $this->iPlayerKillsBoomer   = 0;
     }
 
+    // Set/get the player's name
     public function setName($_sPlayerName){
-        $this->sPlayerName = $_sPlayerName;
+        if(is_string($_sPlayerName)) {
+            $this->sPlayerName = $_sPlayerName;
+            return true;
+        }
+        else return false;
     }
 
     public function getName(){
         return($this->sPlayerName);
     }
 
+    // Set/get the total number of kills made by a player
     public function setKills($_iPlayerKills){
-        $this->iPlayerKills = $_iPlayerKills;
+        if(is_int($_iPlayerKills)) {
+            $this->iPlayerKills = $_iPlayerKills;
+            return true;
+        }
+        else return false;
     }
 
     public function getKills(){
         return($this->iPlayerKills);
     }
 
+    // Set/get the total number of times a player has died
+    public function setDeaths($_iPlayerDeaths){
+        if(is_int($_iPlayerDeaths)) {
+            $this->iPlayerDeaths = $_iPlayerDeaths;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getDeaths(){
+        return($this->iPlayerDeaths);
+    }
+
+    // Set/get a player's total playtime
+    public function setPlayTime($_iPlayerTime){
+        if(is_int($_iPlayerTime)){
+            $this->iPlayerTime = $_iPlayerTime;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getPlayTime(){
+        return($this->iPlayerTime);
+    }
+
+    // Set/get number of shots fired by a player
+    public function setShots($_iPlayerShots){
+        if(is_int($_iPlayerShots)){
+            $this->iPlayerShots = $_iPlayerShots;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getShots(){
+        return($this->iPlayerShots);
+    }
+
+    // Set/get the number of shots that actually hit a target
+    public function setHits($_iPlayerHits){
+        if(is_int($_iPlayerHits)){
+            $this->iPlayerHits = $_iPlayerHits;
+            return true;
+        }
+        return false;
+    }
+
+    public function getHits(){
+        return($this->iPlayerHits);
+    }
+
+    // Set/get the number of headshots made by a player
+    public function setHeadshots($_iPlayerHeadshots){
+        if(is_int($_iPlayerHeadshots)){
+            $this->iPlayerHeadshots = $_iPlayerHeadshots;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getHeadshots(){
+        return($this->iPlayerHeadshots);
+    }
+
+    // Set/get the number of times a player has hurt a member of their team
+    public function setFriendlyFire($_iPlayerFriendlyFire){
+        if(is_int($_iPlayerFriendlyFire)){
+            $this->iPlayerFriendlyFire = $_iPlayerFriendlyFire;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getFriendlyFire(){
+        return($this->iPlayerFriendlyFire);
+    }
+
+    // Set/get the number of times a player has protected a team member
+    public function setTeamProtects($_iPlayeTeamProtects){
+        if(is_int($_iPlayeTeamProtects)){
+            $this->iPlayerTeamProtects = $_iPlayeTeamProtects;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getTeamProtects(){
+        return($this->iPlayerTeamProtects);
+    }
+
+    // Set/get the number of times a player has healed a team member
+    public function setTeamHeals($_iPlayerTeamHeals){
+        if(is_int($_iPlayerTeamHeals)){
+            $this->iPlayerTeamHeals = $_iPlayerTeamHeals;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getTeamHeals(){
+        return($this->iPlayerTeamHeals);
+    }
+
+    // Set/get the number of times a player has defibed a team member
+    public function setTeamDefibs($_iPlayerTeamDefibs){
+        if(is_int($_iPlayerTeamDefibs)){
+            $this->iPlayerTeamDefibs = $_iPlayerTeamDefibs;
+            return true;
+        }
+        else return false;
+    }
+
+    public function getTeamDefibs(){
+        return($this->iPlayerTeamDefibs);
+    }
+
+    // Set/get the next node
     public function setNext($_pNext){
         $this->pNext = $_pNext;
     }
