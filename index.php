c1c1de620562a436fa46cb1d0f97326a40ba74c7
evilmaniac <sauronkitty@users.noreply.github.com>
2015-03-10 00:42:45 -0500

    Encoding for html data added through the use of header() function

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $hPlayerManager = new PlayerManager();
 $hPlayerQueue = $hPlayerManager->getQueue();
 
 $iCounter = 1;
+header('Content-Type: text/html; charset=utf-8');
 while($hPlayerQueue->valid()){
     print"$iCounter - Name: ".$hPlayerQueue->current()->getName()." | Score: ".$hPlayerQueue->current()->getPlayerScore()."<br>\n";
     $hPlayerQueue->next();
