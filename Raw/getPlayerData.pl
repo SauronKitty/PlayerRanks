cd283f50f3c6e8e0c80bea9dd464f7cec29fc7b5
evilmaniac <evilmaniac@eM-SLP>
2015-03-10 00:32:33 -0500

    Updated getPlayerData.pl to parse names correctly

--- Raw/getPlayerData.pl
+++ Raw/getPlayerData.pl
@@ -2,15 +2,13 @@
 
 use XML::Simple;
 use LWP::Simple;
-use Data::Dumper;
+
 use DBI;
 
 use utf8;
 use Text::Unidecode;
 
 &main();
-
-@aScores = ();
 
 sub main(){
 	my $hPlayerList = &getPlayerList();
@@ -20,46 +18,35 @@ sub main(){
 	foreach $_ (keys $hPlayerList->{'playerlist'}->{'player'}){
 		my $hPlayerData = &getPlayerData($_, $hPlayerList);
 		&pushRow($hPlayerData, $hDatabase);
-		#my @aScore = (&getPlayerName($hPlayerData), &generateScore($hPlayerData));
-		#push(@aScores, \@aScore);
 	}
 
 	&disconnectFromDatabase($hDatabase);
-	# my @aSortedScores = sort { $b->[1] <=> $a->[1] } @aScores;
-
-	# my $iListSize = scalar @aSortedScores;
-	# for(my $i=0; $i<$iListSize; $i++){
-	# 	print $aSortedScores[$i][0].": ".$aSortedScores[$i][1]."\n";
-	# }
 }
 
 sub pushRow(){
 	my($hPlayerData, $hDatabase) = @_;
 
-	my $sPlayerName = &getPlayerName($hPlayerData);
-	my $iSpitterKills = &getPlayerSpitterKills($hPlayerData);
-	my $iChargerKills = &getPlayerChargerKills($hPlayerData);
-	my $iSmokerKills = &getPlayerSmokerKills($hPlayerData);
-	my $iHunterKills = &getPlayerHunterKills($hPlayerData);
-	my $iJockeyKills = &getPlayerJockeyKills($hPlayerData);
-	my $iBoomerKills = &getPlayerBoomerKills($hPlayerData);
-	my $iPlayerShots = &getPlayerShots($hPlayerData);
-	my $iPlayerHits = &getPlayerHits($hPlayerData);
+	my $sPlayerName         = &getPlayerName($hPlayerData);
+	my $iSpitterKills       = &getPlayerSpitterKills($hPlayerData);
+	my $iChargerKills       = &getPlayerChargerKills($hPlayerData);
+	my $iSmokerKills        = &getPlayerSmokerKills($hPlayerData);
+	my $iHunterKills        = &getPlayerHunterKills($hPlayerData);
+	my $iJockeyKills        = &getPlayerJockeyKills($hPlayerData);
+	my $iBoomerKills        = &getPlayerBoomerKills($hPlayerData);
+	my $iPlayerShots        = &getPlayerShots($hPlayerData);
+	my $iPlayerHits         = &getPlayerHits($hPlayerData);
 	my $iPlayerFriendlyFire = &getPlayerAction_FriendlyFire($hPlayerData);
-	my $iPlayerProtects = &getPlayerAction_ProtectTeam($hPlayerData);
-	my $iPlayerDefibs = &getPlayerAction_Defib($hPlayerData);
-	my $iPlayerHeals = &getPlayerAction_Heal($hPlayerData);
-	my $iPlayerRevives = &getPlayerAction_Revive($hPlayerData);
-	my $iPlayerPlayTime = &getPlayerTime($hPlayerData);
-	my $iPlayerHeadshots = &getPlayerHeadshots($hPlayerData);
-	my $iPlayerKills = &getPlayerKills($hPlayerData);
-	my $iPlayerDeaths = &getPlayerDeaths($hPlayerData);
-
-    print $sPlayerName."\n";
-	$sPlayerName =~ tr/'*;,//d;
+	my $iPlayerProtects     = &getPlayerAction_ProtectTeam($hPlayerData);
+	my $iPlayerDefibs       = &getPlayerAction_Defib($hPlayerData);
+	my $iPlayerHeals        = &getPlayerAction_Heal($hPlayerData);
+	my $iPlayerRevives      = &getPlayerAction_Revive($hPlayerData);
+	my $iPlayerPlayTime     = &getPlayerTime($hPlayerData);
+	my $iPlayerHeadshots    = &getPlayerHeadshots($hPlayerData);
+	my $iPlayerKills        = &getPlayerKills($hPlayerData);
+	my $iPlayerDeaths       = &getPlayerDeaths($hPlayerData);
 
 	my $hStatement = $hDatabase->prepare(
-			"INSERT INTO Players (
+			qq(INSERT INTO Players (
 				PlayerName,
 				PlayerKills,
 				PlayerDeaths,
@@ -79,31 +66,33 @@ sub pushRow(){
 				PlayerKillsJockey,
 				PlayerKillsBoomer 
 			) VALUES (
-				\'$sPlayerName\', 
-				$iPlayerKills,
-				$iPlayerDeaths,
-				$iPlayerPlayTime,
-				$iPlayerShots,
-				$iPlayerHits,
-				$iPlayerHeadshots,
-				$iPlayerFriendlyFire,
-				$iPlayerProtects,
-				$iPlayerHeals,
-				$iPlayerDefibs,
-				$iPlayerRevives,
-				$iSpitterKills,
-				$iChargerKills,
-				$iSmokerKills,
-				$iHunterKills,
-				$iJockeyKills,
-				$iBoomerKills)");
-		$hStatement->execute();
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?,
+				?)));
+		$hStatement->execute($sPlayerName, $iPlayerKills, $iPlayerDeaths, $iPlayerPlayTime, $iPlayerShots, $iPlayerHits,
+		$iPlayerHeadshots, $iPlayerFriendlyFire, $iPlayerProtects, $iPlayerHeals, $iPlayerDefibs, $iPlayerRevives,
+		$iSpitterKills, $iChargerKills, $iSmokerKills, $iHunterKills, $iJockeyKills, $iBoomerKills);
 		return;
 }
 
 sub connectToDatabase(){
 	unlink("playerranks.db");
-	$hDatabase = DBI->connect("dbi:SQLite:dbname=playerranks.db","","",{ RaiseError => 1 },);
+	$hDatabase = DBI->connect("dbi:SQLite:dbname=playerranks.db",qq(),qq(),{ RaiseError => 1 },);
 	return $hDatabase;
 }
 
@@ -156,13 +145,10 @@ sub getPlayerData(){
 	my ($sPlayerID, $hPlayerList) = @_;
 	my $sData = get "http://evilmania.gameme.com/api/playerinfo/l4dii2/".$sPlayerID."/extended";
 
-	$sData =~ s/([^[:ascii:]]+)/unidecode($1)/ge;
 	$sData =~ s/\<awards\>.*\<\/awards\>//ig;
 
 	my $hXML = XML::Simple->new();
 	my $hashRefrence = $hXML->XMLin($sData, KeyAttr => {action => 'code'});
-
-	#print Dumper($hashRefrence);
 
 	return($hashRefrence)
 }
@@ -238,8 +224,6 @@ sub getPlayerBoomerKills(){
 	return($hPlayerData->{'playerinfo'}->{'player'}->{'actions'}->{'action'}->{'killed_exploding'}->{'achieved'});
 }
 
-#
-
 sub getPlayerAction_ProtectTeam(){
  	my ($hPlayerData) = @_;
  	return($hPlayerData->{'playerinfo'}->{'player'}->{'actions'}->{'action'}->{'protect_teammate'}->{'achieved'});
@@ -265,81 +249,6 @@ sub getPlayerAction_Revive(){
 sub generateSIKillsScore(){
 	my ($hPlayerData) = @_;
 	return((&calcTotalSIKills($hPlayerData)/&secondsToMinutes(&getPlayerTime($hPlayerData)))*500);
-}
-
-sub generateScore(){
-	my ($hPlayerData) = @_;
-	my $iScoreKillsPerMinute = &calcKillsPerMinute($hPlayerData)*33.34;
-	my $iScoreKillsToDeath = &calcKilltoDeathRatio($hPlayerData);
-	my $iScoreHeadshots = &calcHeadShotPercentage($hPlayerData);
-	my $iScoreTeamAssists = &calcTeamAssists($hPlayerData);
-	my $iScoreAccuracy = &calcAccuracy($hPlayerData);
-
-	print &getPlayerName($hPlayerData)."\n";
-	print "\t KPM Score: ".$iScoreKillsPerMinute."\n";
-	print "\t KD Ratio Score: ".500*$iScoreKillsToDeath."\n";
-	print "\t Headshots Score: ".500*$iScoreHeadshots."\n";
-	print "\t Team Assists Score: ".500*$iScoreTeamAssists."\n";
-	print "\t Accuracy Score: ".500*$iScoreAccuracy."\n";
-	print "\t SI Kill Score: ".&generateSIKillsScore($hPlayerData)."\n";
-
-	my $iScore = $iScoreKillsPerMinute + ($iScoreKillsToDeath + $iScoreHeadshots + $iScoreTeamAssists + $iScoreAccuracy)*500;
-	return($iScore+&generateSIKillsScore($hPlayerData));
-
-}
-
-# Calculate
-
-sub calcTotalSIKills(){
-	my ($hPlayerData) = @_;
-	my $iSpitterKills = &getPlayerSpitterKills($hPlayerData);
-	my $iChargerKills = &getPlayerChargerKills($hPlayerData);
-	my $iSmokerKills = &getPlayerSmokerKills($hPlayerData);
-	my $iHunterKills = &getPlayerHunterKills($hPlayerData);
-	my $iJockeyKills = &getPlayerJockeyKills($hPlayerData);
-	my $iBoomerKills = &getPlayerBoomerKills($hPlayerData);
-
-	return($iSpitterKills+$iChargerKills+$iSmokerKills+$iHunterKills+$iJockeyKills+$iBoomerKills);
-}
-
-sub calcAccuracy(){
-	my ($hPlayerData) = @_;
-	my $iPlayerShots = &getPlayerShots($hPlayerData);
-	my $iPlayerHits = &getPlayerHits($hPlayerData);
-	return($iPlayerHits/$iPlayerShots);
-}
-
-sub calcTeamAssists(){
-	my ($hPlayerData) = @_;
-	my $iPlayerDefibs = &getPlayerAction_Defib($hPlayerData);
-	my $iPlayerHeals = &getPlayerAction_Heal($hPlayerData);
-	my $iPlayerRevives = &getPlayerAction_Revive($hPlayerData);
-	my $iPlayerPlayTime = &secondsToMinutes(&getPlayerTime($hPlayerData));
-	return(($iPlayerDefibs+$iPlayerHeals+$iPlayerRevives)/$iPlayerPlayTime);
-}
-
-sub calcKillsPerMinute() {
-	my ($hPlayerData) = @_;
-	my $iPlayerPlayTime = &secondsToMinutes(&getPlayerTime($hPlayerData));
-	my $iPlayerKills = &getPlayerKills($hPlayerData);
-
-	return($iPlayerKills/$iPlayerPlayTime);
-}
-
-sub calcHeadShotPercentage(){
-	my ($hPlayerData) = @_;
-	my $iPlayerHeadshots = &getPlayerHeadshots($hPlayerData);
-	my $iPlayerKills = &getPlayerKills($hPlayerData);
-
-	return($iPlayerHeadshots/$iPlayerKills);
-}
-
-sub calcKilltoDeathRatio(){
-	my ($hPlayerData) = @_;
-	my $iPlayerKills = &getPlayerKills($hPlayerData);
-	my $iPlayerDeaths = &getPlayerDeaths($hPlayerData);
-
-	return(($iPlayerKills/$iPlayerDeaths)/100);
 }
 
 # Other
