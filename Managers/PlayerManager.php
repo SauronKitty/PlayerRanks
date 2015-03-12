4c385c1188d5babdfa1df41115e5f39eea40adcc
evilmaniac <sauronkitty@users.noreply.github.com>
2015-03-11 22:12:29 -0500

    Split up PlayerManager into smaller functions for more flexibility

--- Managers/PlayerManager.php
+++ Managers/PlayerManager.php
@@ -2,51 +2,73 @@
 
 class PlayerManager {
     private $pPlayerQueue;
+    private $hDatabase;
 
     public function __construct(){
-        $hDatabase = new Database("./Raw/playerranks.db");
-        $this->pPlayerQueue = new PlayerQueue();
-        $this->populateQueue($hDatabase);
+        $this->hDatabase = new Database("./Raw/playerranks.db");
     }
 
-    private function populateQueue(Database &$_hDatabase){
-        $hResponse = $_hDatabase->execQuery("SELECT * From Players");
-        foreach($hResponse as $hRow){
-            $hPlayerNode = new PlayerNode();
+    public function populateQueue(){
+        $this->pPlayerQueue = new PlayerQueue(); // Create the queue
 
-            // Parse each database row into a PlayerNode
-            $hPlayerNode->setName($hRow['PlayerName']);
-            $hPlayerNode->setSteamId($hRow['PlayerSteam']);
-            $hPlayerNode->setGameMeId($hRow['PlayerId']);
-            $hPlayerNode->setKills($hRow['PlayerKills']);
-            $hPlayerNode->setDeaths($hRow['PlayerDeaths']);
-            $hPlayerNode->setPlayTime($hRow['PlayerTime']);
-            $hPlayerNode->setShots($hRow['PlayerShots']);
-            $hPlayerNode->setHits($hRow['PlayerHits']);
-            $hPlayerNode->setHeadshots($hRow['PlayerHeadshots']);
-            $hPlayerNode->setFriendlyFire($hRow['PlayerFriendlyFire']);
-            $hPlayerNode->setTeamProtects($hRow['PlayerTeamProtects']);
-            $hPlayerNode->setTeamHeals($hRow['PlayerTeamHeals']);
-            $hPlayerNode->setTeamDefibs($hRow['PlayerTeamDefibs']);
-            $hPlayerNode->setTeamRevives($hRow['PlayerTeamRevives']);
-            $hPlayerNode->setKillsSpitter($hRow['PlayerKillsSpitter']);
-            $hPlayerNode->setKillsCharger(($hRow['PlayerKillsCharger']));
-            $hPlayerNode->setKillsSmoker($hRow['PlayerKillsSmoker']);
-            $hPlayerNode->setKillsHunter($hRow['PlayerKillsHunter']);
-            $hPlayerNode->setKillsJockey($hRow['PlayerKillsJockey']);
-            $hPlayerNode->setKillsBoomer($hRow['PlayerKillsBoomer']);
+        $hResponse = $this->hDatabase->getDatabase()->prepare("SELECT * FROM Players");
+        if($hResponse->execute())
+            foreach($hResponse as $hRow)
+                $this->queuePlayer($this->loadPlayer($hRow));
 
-            // ScoreCalculator will take the $hPlayerNode by refrence and automatically calculate the score
-            // based on the data passed earlier. ScoreCalculator will automatically also call the
-            // $hPlayerNode->setPlayerScore() and update the node's score
-            new ScoreCalculator($hPlayerNode);
-            // Insert $hPlayerNode into the PriorityQueue with the calculated score as the Node's priority
-            $this->pPlayerQueue->addPlayer($hPlayerNode, $hPlayerNode->getPlayerScore());
-        }
+        return;
+    }
+
+    public function getPlayerByName($_sPlayerName){
+        $hResponse = $this->hDatabase->getDatabase()->prepare("SELECT * FROM Players WHERE PlayerName=?");
+        $hResponse->execute(array($_sPlayerName));
+
+        // TODO: Check to make sure reply is not null
+        $hResponse = $hResponse->fetch(0);
+        return ($this->loadPlayer($hResponse));
+        //return null;
+    }
+
+    private function &loadPlayer(&$_hRow){
+        $hPlayerNode = new PlayerNode();
+
+        // Parse each database row into a PlayerNode
+        $hPlayerNode->setName($_hRow['PlayerName']);
+        $hPlayerNode->setSteamId($_hRow['PlayerSteam']);
+        $hPlayerNode->setGameMeId($_hRow['PlayerId']);
+        $hPlayerNode->setKills($_hRow['PlayerKills']);
+        $hPlayerNode->setDeaths($_hRow['PlayerDeaths']);
+        $hPlayerNode->setPlayTime($_hRow['PlayerTime']);
+        $hPlayerNode->setShots($_hRow['PlayerShots']);
+        $hPlayerNode->setHits($_hRow['PlayerHits']);
+        $hPlayerNode->setHeadshots($_hRow['PlayerHeadshots']);
+        $hPlayerNode->setFriendlyFire($_hRow['PlayerFriendlyFire']);
+        $hPlayerNode->setTeamProtects($_hRow['PlayerTeamProtects']);
+        $hPlayerNode->setTeamHeals($_hRow['PlayerTeamHeals']);
+        $hPlayerNode->setTeamDefibs($_hRow['PlayerTeamDefibs']);
+        $hPlayerNode->setTeamRevives($_hRow['PlayerTeamRevives']);
+        $hPlayerNode->setKillsSpitter($_hRow['PlayerKillsSpitter']);
+        $hPlayerNode->setKillsCharger(($_hRow['PlayerKillsCharger']));
+        $hPlayerNode->setKillsSmoker($_hRow['PlayerKillsSmoker']);
+        $hPlayerNode->setKillsHunter($_hRow['PlayerKillsHunter']);
+        $hPlayerNode->setKillsJockey($_hRow['PlayerKillsJockey']);
+        $hPlayerNode->setKillsBoomer($_hRow['PlayerKillsBoomer']);
+
+        // ScoreCalculator will take the $hPlayerNode by reference and automatically calculate the score
+        // based on the data passed earlier. ScoreCalculator will automatically also call the
+        // $hPlayerNode->setPlayerScore() and update the node's score
+        new ScoreCalculator($hPlayerNode);
+        return($hPlayerNode);
+    }
+
+    private function queuePlayer(PlayerNode &$_hPlayerNode){
+        // Insert $hPlayerNode into the PriorityQueue with the calculated score as the Node's priority
+        $this->pPlayerQueue->addPlayer($_hPlayerNode, $_hPlayerNode->getPlayerScore());
+        return;
     }
 
     public function getQueue(){
-        $hQueueRefrence = &$this->pPlayerQueue->getQueue();
-        return($hQueueRefrence);
+        $hQueueReference = &$this->pPlayerQueue->getQueue();
+        return($hQueueReference);
     }
 }
